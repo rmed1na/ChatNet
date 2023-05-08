7a118f3c630224285bc498a5c9539162625019ce
Rolando Medina <rolando.ms@outlook.com>
2023-05-08 01:38:02 -0500

    feat: add user repository

---
+++ ChatNet.Data/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using ChatNet.Data.Context;
+using ChatNet.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatNet.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IChatNetContext _ctx;
+
+        public UserRepository(IChatNetContext ctx)
+            => _ctx = ctx;
+
+        public async Task AddAsync(User entity)
+        {
+            await _ctx.Users.AddAsync(entity);
+            await _ctx.SaveChangesAsync();
+        }
+
+        public async Task<User?> GetAsync(string username)
+            => await _ctx.Users.FirstOrDefaultAsync(x => x.Username.ToLower() == username.ToLower());
+
+        public async Task<bool> UsernameExistsAsync(string username)
+            => await _ctx.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower());
+    }
+}
---
+++ ChatNet/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using ChatNet.Data.Context;
+using ChatNet.Data.Repositories;
+
+namespace ChatNet
+{
+    public static class DependencyInjection
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            #region Scoped
+            services.AddScoped<IChatNetContext, ChatNetContext>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            #endregion
+        }
+    }
+}
